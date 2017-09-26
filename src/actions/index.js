0a9e965e0183d9a0ef862a19d25ca8de49f88fe2
Michelle Poterek <poterekm@gmail.com>
2017-09-26 11:08:30 -0700

    add ability to console log joke from user input search

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,13 +4,17 @@ import * as types from "./../constants/ActionTypes";
 export function fetchJoke(searchWord) {
   return function (dispatch) {
     searchWord = searchWord.replace(" ", "_");
-    return fetch("https://icanhazdadjoke.com/search?term=" + searchWord).
-    then(
+    return fetch("https://icanhazdadjoke.com/search?term=" + searchWord + "&limit=1",
+    {
+      headers : {
+        'Accept': 'application/json'
+      }}).then(
       response => response.json(),
       error => console.log("This is an error message you've created in your index.js file", error)
     ).then(function(json) {
-      if (json.results.joke > 0) {
-        console.log("Yes, we've recieved a joke!");
+      if (json.total_jokes > 0) {
+        const joke = json.results[0].joke
+        console.log(joke);
       }
     })
   }
